9ab50ec86451e633f0d450c55198ae67b72a6019
James Qiu <91434864+firuz727@users.noreply.github.com>
2025-11-07 14:45:02 -0500

    Update HMAC key and fix formatting issues

--- HMAC.cpp
+++ HMAC.cpp
@@ -4,8 +4,8 @@
 #include "Packet.h"
 
 static const SecretKey key = {
-  .key = "",
-  .length = strlen("KEY")
+  .key = "PUTKEYHERE",
+  .length = strlen("PUTKEYHERE")
 };
 
 SHA256 sha;
@@ -21,4 +21,5 @@ void generateHMAC(Packet* pkt) {
   sha.resetHMAC(secretKey, keyLen);                       // Step 1: prepare inner hash with ipad
   sha.update(message, messageLen);                        // Step 2: feed in your message
   sha.finalizeHMAC(secretKey, keyLen, pkt->hmac, 32);  // Step 3: finalize with opad and get HMAC
+
 }
